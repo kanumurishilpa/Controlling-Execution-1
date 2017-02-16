7ed34ada3fa9cfc488eb853a7a307c2d0a7211bb
zipcoder <kanumurishilpa@gmail.com>
2017-02-16 15:32:11 -0500

    stupid messages..

---
+++ src/main/java/kanumuri/wolfe/controllingexecution/Prompt.java
@@ -0,0 +1,10 @@
+package kanumuri.wolfe.controllingexecution;
+
+/**
+ * Created by zipcoder on 2/16/17.
+ */
+public class Prompt {
+    public void prompt(){
+        System.out.println("Please enter an integer: ");
+    }
+}
--- src/main/java/kanumuri/wolfe/controllingexecution/UserInput.java
+++ src/main/java/kanumuri/wolfe/controllingexecution/UserInput.java
@@ -6,9 +6,12 @@ import java.util.Scanner;
  * Created by zipcoder on 2/16/17.
  */
 public class UserInput {
+    Prompt p = new Prompt();
     Scanner kb = new Scanner(System.in);
 
     public int getUserInput(){
-        return kb.nextInt();
+        p.prompt();
+        int input = kb.nextInt();
+        return input;
     }
 }
